9ec0a2b659631434ff45b74cbe25e0b0c0abd63a
JDevSant <juansant.dev@gmail.com>
2026-09-15 14:06:02 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -369,21 +369,4 @@ document.addEventListener('keydown', e => {
 
 document.addEventListener('DOMContentLoaded', () => {
     renderCourses();
-
-    // Botoes de inicio com listener direto para maior compatibilidade mobile.
-    ['startNav', 'startHero', 'startCourses', 'restartQuiz'].forEach(id => {
-        const button = $(id);
-
-        if (button) {
-            button.addEventListener('click', startQuiz);
-        }
-    });
 });
-
-// Mantem as funcoes acessiveis aos botoes e a navegacao do HTML.
-window.startQuiz = startQuiz;
-window.nextQuestion = nextQuestion;
-window.prevQuestion = prevQuestion;
-window.goHome = goHome;
-window.openCourse = openCourse;
-window.closeModal = closeModal;
